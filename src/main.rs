5de103ba0d41cccb3b8863f2f27f9932c8d8d36d
Sebastian <johancs.mm@gmail.com>
2023-08-25 20:33:39 -0500

    chore: variables sintax

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
+
+    // comentarios con rust
+    /*
+    Multiple comentarios
+    con rust
+    s
+    da
+    asd
+     */
+    println!("Hola Rust en español"); // ";" muy importante si no el compilador molestará y no lo va compilar.
+
+    // #[warn(unused_doc_domments)] - error de comentarios
     println!("Hello, world!");
+
+    // Como se escribe una variable ?
+
+    // #[warn(ununsed_variable)] -- Cuando no se usa una variable
+    let my_string = "Hello World";
+
+    println!("Esta es mi variable: {}", my_string);
 }
